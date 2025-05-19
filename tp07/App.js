9794f442f034e61070ea333c15ef8fa5117ba988
EZEQUIEL KALIK <48674479@ort>
2025-05-19 11:54:48 -0300

    inicio

--- tp07/App.js
+++ tp07/App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TextInput, TouchableOpacity, StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <Text style={styles.title}>Iniciar Sesión</Text>
+
+       <TextInput
+         style={styles.input}
+         placeholder="Usuario"
+        placeholderTextColor="#aaa"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        placeholderTextColor="#aaa"
+        secureTextEntry
+      />
+
+     <TouchableOpacity style={styles.button}>
+     <Text style={styles.buttonText}>Ingresar</Text>
+     </TouchableOpacity>
+
+     <StatusBar style="auto" />
     </View>
   );
 }
@@ -16,5 +34,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    marginBottom: 20,
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    marginTop: 10,
+    width: '100%',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
