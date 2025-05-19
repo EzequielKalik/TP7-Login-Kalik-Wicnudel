4bdc5e63b70c230118294f330fe15bb26cbbc260
Android Studio <devandroid@ort>
2025-05-19 12:10:27 -0300

    esta encaminado

--- tp07/App.js
+++ tp07/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , TextInput, TouchableOpacity, StatusBar } from 'react-native';
+import { StyleSheet, Text, View , TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
